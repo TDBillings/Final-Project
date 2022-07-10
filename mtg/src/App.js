56f2a19cceb2bbfb3a2437e1c23f4475f8dfcf21
Trevor Billings <Deafstroked@yahoo.com>
2022-07-09 22:43:37 -0400

    React app live

---
+++ mtg/src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Pages/Home';
+import './App.css';
+
+// function Home() {
+//   return (
+//     <>
+//       <main>
+//         <h2>Welcome to the homepage!</h2>
+//         <p>You can do this, I believe in you.</p>
+//       </main>
+//       <nav>
+//         <Link to="/about">About</Link>
+//       </nav>
+//     </>
+//   );
+// }
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
